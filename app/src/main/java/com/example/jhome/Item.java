bc2d0df64383c63b0bb8c531aabf407f77a2ee3a
savain <SavainB>
2023-08-12 16:33:05 +0200

    ajout de tout les fihcier

---
+++ app/src/main/java/com/example/jhome/Item.java
@@ -0,0 +1,9 @@
+package com.example.jhome;
+
+import android.graphics.drawable.Drawable;
+
+public class Item {
+    CharSequence label; // nom du ficher dossier
+    CharSequence name;// nom de lapp
+    Drawable icon; // licone
+}
