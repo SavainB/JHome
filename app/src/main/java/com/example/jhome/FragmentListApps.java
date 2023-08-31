97d5298ae0d6b9b67946f19275dc5497c03bb85a
savain <SavainB>
2023-08-31 12:45:47 +0200

    remplacement de tout les activity par des fragments ajout de la fonctionnaliter de recherche

--- app/src/main/java/com/example/jhome/FragmentListApps.java
+++ app/src/main/java/com/example/jhome/FragmentListApps.java
@@ -1,12 +1,14 @@
 package com.example.jhome;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -23,19 +25,72 @@ import java.util.List;
 import java.util.Locale;
 import java.util.regex.Pattern;
 
-public class AppListActivity extends AppCompatActivity {
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FragmentListApps#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class FragmentListApps extends Fragment {
     private PackageManager manager;
     private List<Item> apps;
     private ListView list;
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FragmentListApps() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment FragmentListApps.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FragmentListApps newInstance(String param1, String param2) {
+        FragmentListApps fragment = new FragmentListApps();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_list);
-        /*loadApps();
-        loadListView();
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_list_apps, container, false);
+
+        Bundle args = getArguments();
+        if (args != null) {
+            String sharedData = args.getString("key");
+            Button button = rootView.findViewById(R.id.bouton);
+            button.setText(sharedData);
+        }
+
+        loadApps();
+        loadListView(rootView);
         addClickListener();
-        EditText editText = (EditText) findViewById(R.id.edit);
-        Button button = (Button) findViewById(R.id.bouton);
+        EditText editText = (EditText) rootView.findViewById(R.id.edit);
+        Button button = (Button) rootView.findViewById(R.id.bouton);
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
@@ -44,7 +99,7 @@ public class AppListActivity extends AppCompatActivity {
                 for (Item item: apps){
                     String nameApplication = removeAccents(item.name.toString().toLowerCase(Locale.FRENCH));
                     if (EditTextText.contentEquals(nameApplication) ){
-                        ImageView singleItemImage = findViewById(R.id.singleItemImage);
+                        ImageView singleItemImage = rootView.findViewById(R.id.singleItemImage);
                         singleItemImage.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View view) {
@@ -59,10 +114,12 @@ public class AppListActivity extends AppCompatActivity {
                 }
             }
         });
-    */
+
+        return rootView;
     }
+
     private void loadApps(){
-        manager = getPackageManager();
+        manager = requireContext().getPackageManager();
         apps = new ArrayList<>();
         Intent i = new Intent(Intent.ACTION_MAIN, null);
         i.addCategory(Intent.CATEGORY_LAUNCHER);
@@ -76,9 +133,9 @@ public class AppListActivity extends AppCompatActivity {
         }
 
     }
-    private void loadListView(){
-        list = (ListView) findViewById(R.id.list_item);
-        ArrayAdapter<Item> adapter = new ArrayAdapter<Item>(this,R.layout.item,apps){
+    private void loadListView(View rootView){
+        list = (ListView) rootView.findViewById(R.id.list_item);
+        ArrayAdapter<Item> adapter = new ArrayAdapter<Item>(requireContext(),R.layout.item,apps){
             @NonNull
             @Override
             public View getView(int position,@NonNull View convertView,@NonNull ViewGroup parent){
@@ -91,23 +148,24 @@ public class AppListActivity extends AppCompatActivity {
                 appName.setText(apps.get(position).name);
                 return convertView;
             }
-
         };
         System.out.println(adapter);
         list.setAdapter(adapter);
     }
+
     private void addClickListener(){
-    list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Intent i = manager.getLaunchIntentForPackage(apps.get(position).label.toString());
-            startActivity(i);
-        }
-    });
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = manager.getLaunchIntentForPackage(apps.get(position).label.toString());
+                startActivity(i);
+            }
+        });
     }
     public static String removeAccents(String input) {
         String normalized = Normalizer.normalize(input, Normalizer.Form.NFD);
         Pattern pattern = Pattern.compile("\\p{InCombiningDiacriticalMarks}+");
         return pattern.matcher(normalized).replaceAll("");
     }
+
 }
--- app/src/main/java/com/example/jhome/MainActivity.java
+++ app/src/main/java/com/example/jhome/MainActivity.java
@@ -1,30 +1,45 @@
 package com.example.jhome;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),AppListActivity.class);
-                startActivity(intent);
-            }
-        });
+        //loadApps();
+        //loadListView();
+        //addClickListener();
+        FragmentListApps fragment2 = new FragmentListApps();
+        Bundle args = new Bundle();
+        args.putString("key", "ca marche de ouf");
+        fragment2.setArguments(args);
 
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
     }
-    @Override
-    public void onBackPressed(){
-        // N?E FAIT RIEN
-    }
+
+
+
+
 }
